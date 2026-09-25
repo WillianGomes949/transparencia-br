06a6e23cdf2a704da4bc454954747f92c7c6f429
Willian Gomes <146601824+WillianGomes949@users.noreply.github.com>
2026-09-25 13:08:34 -0300

    conf iniciais

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "2mb",
+    },
+  },
+  images: {
+    domains: ["portaldatransparencia.gov.br"],
+  },
 };
 
 export default nextConfig;
